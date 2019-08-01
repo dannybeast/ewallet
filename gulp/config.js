f2dd1b04d51428d3a1467894dfa616d4ccafaf00
Danil Barsukov <d.barsukov@danycom.ru>
2019-08-01 16:51:35 +0300

    add

--- gulp/config.js
+++ gulp/config.js
@@ -1,53 +1,57 @@
-var util = require('gulp-util');
+import util from 'gulp-util';
 
-var production = util.env.production || util.env.prod || false;
-var destPath = 'build';
+const production = util.env.production || util.env.prod || util.env._.indexOf('build') !== -1 || false;
+const destPath = 'build';
 
-var config = {
+const config = {
   env: 'development',
-  production: production,
+  production,
 
   src: {
     root: 'src',
-    templates: 'src/blocks',
-    templatesData: 'src/blocks/data',
+    templates: 'src/templates',
+    templatesData: 'src/templates/data',
     pagelist: 'src/index.yaml',
-    sass: 'src/blocks',
+    sass: 'src/sass',
+    // path for sass files that will be generated automatically via some of tasks
+    sassGen: 'src/sass/generated',
     js: 'src/js',
-    css: 'src/css',
     img: 'src/img',
-    svg: 'src/img/icons/svg',
+    svg: 'src/img/svg',
     icons: 'src/icons',
     // path to png sources for sprite:png task
-    iconsPng: 'src/img/icons',
+    iconsPng: 'src/icons',
     // path to svg sources for sprite:svg task
-    iconsSvg: 'src/img/icons',
+    iconsSvg: 'src/icons',
     // path to svg sources for iconfont task
     iconsFont: 'src/icons',
-    fonts: 'src/fonts/*',
-    lib: 'src/lib'
+    fonts: 'src/fonts',
+    lib: 'src/lib',
+    data: 'src/data'
   },
   dest: {
     root: destPath,
     html: destPath,
-    css: destPath + '/css',
-    js: destPath + '/js',
-    img: destPath + '/img',
-    fonts: destPath + '/fonts',
-    lib: destPath + '/lib'
+    css: `${destPath  }/css`,
+    js: `${destPath  }/js`,
+    img: `${destPath  }/img`,
+    fonts: `${destPath  }/fonts`,
+    lib: `${destPath  }/lib`,
+    data: `${destPath  }/data`,
+    icons: `${destPath  }/img/icons`
   },
 
-  setEnv: function(env) {
+  setEnv(env) {
     if (typeof env !== 'string') return;
     this.env = env;
     this.production = env === 'production';
     process.env.NODE_ENV = env;
   },
 
-  logEnv: function() {
+  logEnv() {
     util.log(
       'Environment:',
-      util.colors.white.bgRed(' ' + process.env.NODE_ENV + ' ')
+      util.colors.white.bgRed(` ${  process.env.NODE_ENV  } `)
     );
   },
 
